35fbebf530d8f9b738fafc4becd698940b33f169
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-21 13:37:08 -0800

    adds two pangram solutions

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -4,21 +4,17 @@
 //
 
 export const isPangram = (str) => {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
   str.split('').forEach((char) => {
-    let i = alphabet.indexOf(char);
+    let i = alphabet.indexOf(char.toLowerCase());
     if (i > -1) {
-      alphabet.replace(alphabet[i], '');
+      alphabet = alphabet.replace(alphabet[i], '');
     }
   });
   return alphabet.length === 0;
+  // let newStr = str.toLowerCase()
+  // for (let i = 0; i < alphabet.length; i++) {
+  //   if (!newStr.includes(alphabet[i].toLowerCase())) return false;
+  // }
+  // return true;
 };
-
-// I recognize that this solution does not work, but I am submitting it anyway, because I think the logic is right, and I'm not sure why it's not working, so I want to stick with it.
-
-// Here's the pseudocode
-
-// split string into an array to iterate over it
-// if char exists in alphabet, replace that index of alphabet with nothing
-// do this for each char of str
-// if alphabet is empty at the end of the forEach loop, return true. else return false
