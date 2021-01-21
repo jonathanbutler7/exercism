4ea3b56edd7734fca6c13ba3d7988fd5ae82c6bf
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-20 20:39:38 -0800

    adds pangram solution

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPangram = (str) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  str.split('').forEach((char) => {
+    let i = alphabet.indexOf(char);
+    if (i > -1) {
+      alphabet.replace(alphabet[i], '');
+    }
+  });
+  return alphabet.length === 0;
 };
+
+// I recognize that this solution does not work, but I am submitting it anyway, because I think the logic is right, and I'm not sure why it's not working, so I want to stick with it.
+
+// Here's the pseudocode
+
+// split string into an array to iterate over it
+// if char exists in alphabet, replace that index of alphabet with nothing
+// do this for each char of str
+// if alphabet is empty at the end of the forEach loop, return true. else return false
